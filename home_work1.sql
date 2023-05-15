8edf742e5c9072d3565485824a8c013c4666a18f
aanyamarija <124683108+aanyamarija@users.noreply.github.com>
2023-05-15 18:25:46 +0200

    Add files via upload

---
+++ home_work1.sql
@@ -0,0 +1,10 @@
+-- home work 
+-- select * from employees where department_id = 90;
+
+-- select * from employees where salary > 5000;
+
+-- select * from employees where last_name like "%L";
+
+-- select * from departments where location_id =1700;
+
+-- select * from locations where country_id like "US";
